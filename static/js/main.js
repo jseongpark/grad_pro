9b6ee82978d5567e42ab597fa560f44a09037e49
jseongpark <jaeseongpark11@gmail.com>
2022-11-14 04:48:32 +0900

    loading bar

--- static/js/main.js
+++ static/js/main.js
@@ -9,37 +9,48 @@ const happy = document.getElementById('happy');
 const good = document.getElementById('good');
 const angry = document.getElementById('angry');
 const prob = document.getElementById("probability");
+const loader = $("div.loader");
+function test() {
+    _loadPage();
+    var text = $('#chat').val();
 
-function test(){
-            var text = $('#chat').val();
-            
-            $.ajax({
-                    type : 'POST',                                 
-                    url : 'http://127.0.0.1:5000/model',
-                    data : {
-                           chat:text                       
-                    },
-                    dataType : 'JSON',
-                    success : function(result){
-                            const probability = JSON.parse(result)
-                            prob.innerText = probability
-                            if(probability > 60){
-                                happy.classList.add('invisible')
-                                good.classList.add('invisible')
-                                angry.classList.remove('invisible')
-                            } else if(probability >40){
-                                happy.classList.add('invisible')
-                                good.classList.remove('invisible')
-                                angry.classList.add('invisible')
-                            }else{
-                                happy.classList.remove('invisible')
-                                good.classList.add('invisible')
-                                angry.classList.add('invisible')
-                            }
+    $.ajax({
+        type: 'POST',
+        url: 'http://127.0.0.1:5000/model',
+        data: {
+            chat: text
+        },
+        dataType: 'JSON',
+        success: function (result) {
+            const probability = JSON.parse(result)
+            prob.innerText = probability
+            if (probability > 60) {
+                happy.classList.add('invisible')
+                good.classList.add('invisible')
+                angry.classList.remove('invisible')
+            } else if (probability > 40) {
+                happy.classList.add('invisible')
+                good.classList.remove('invisible')
+                angry.classList.add('invisible')
+            } else {
+                happy.classList.remove('invisible')
+                good.classList.add('invisible')
+                angry.classList.add('invisible')
+            }
+            _showPage();
+        },
+        error: function (xtr, status, error) {
+            alert(xtr + ":" + status + ":" + error);
+        }
+    });
+}
 
-                    },
-                    error : function(xtr,status,error){
-                            alert(xtr +":"+status+":"+error);
-                    }
-            });
-    }
+
+
+var _showPage = function () {
+    loader.css("display", "none");
+};
+
+var _loadPage = function () {
+    loader.css("display", "visible");
+};
